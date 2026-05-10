f69def746d06ff58cd2a252e68726f552f258a90
Badi1298 <serbandavid83@yahoo.com>
2026-05-10 17:05:43 +0300

    feat: add signup form component and route for user registration

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { SignupForm } from "#/components/sigup-form";
 
-export const Route = createFileRoute('/signup')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/signup")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/signup"!</div>
+	return (
+		<main className="flex h-screen w-screen items-center justify-center">
+			<SignupForm />
+		</main>
+	);
 }
