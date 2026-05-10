a1268215e5ca557ba98706c4002c7acd6c1d528a
Badi1298 <serbandavid83@yahoo.com>
2026-05-10 13:21:05 +0300

    feat: implement login form and UI components for user authentication

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { LoginForm } from "#/components/login-form";
 
-export const Route = createFileRoute('/login')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/login")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/login"!</div>
+	return (
+		<main className="flex h-screen w-screen items-center justify-center">
+			<LoginForm />
+		</main>
+	);
 }
